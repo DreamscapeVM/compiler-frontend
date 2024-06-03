9d54d645482485f238e5e8d7bdb8cb705935d315
Joo Hyoung Cha <aoikazto@naver.com>
2024-06-03 21:45:07 +0900

    define: tokenizer

--- inc/tokenizer/token.h
+++ inc/tokenizer/token.h
@@ -1,17 +1,19 @@
 #ifndef __INCLUDE_TOKENIZER_TOKEN_H
 #define __INCLUDE_TOKENIZER_TOKEN_H
 
-enum class token { 
-    operand,
-    name,
-    something_start,
-    define_constant,
-    define_variable_mutable,
-    define_variable_unmutable,
-    start_data_type,
-    data_type,
-    start_array,
-    end_array
+#include <cstdint>
+
+enum class token : uint8_t { 
+    error,           // for NULL
+    eof,             // success and work done.
+    operand,         // +, -, *, /
+    name,            // name of func, parm or var
+    something_start, // ;
+    define_constant, // num
+    start_data_type, // :
+    array,           // [, ]
+    reserved_word,   // while, for, if, etc ...
+    comment,         // # comment
 };
 
 #endif // __INCLUDE_TOKENIZER_TOKEN_H
--- inc/tokenizer/tokenizer.h
+++ inc/tokenizer/tokenizer.h
@@ -11,17 +11,44 @@
 
 class tokenizer { 
 private:
-    std::vector<uint8_t> data;
+    using memory_data = std::vector<uint8_t>;
+    memory_data data;
+    memory_data::const_iterator iter;
 
 public:
     std::vector<uint8_t> read_from_file(const std::string& filename) const;
 
     void set_data(const std::vector<uint8_t>& data) { 
         this->data = data;
+        this->iter = this->data.begin();
     }
 
-    std::tuple<token, std::string> get_token() const { 
-        return std::make_tuple(token::data_type, "");
+    std::tuple<token, std::string> get_token() { 
+        if (iter == data.end()) { 
+            return std::make_tuple(token::eof, "");
+        }
+
+        std::string result;
+        
+        while (iter == data.end()) { 
+            char cur_data = *iter;
+            // this is comments. skip until find a new line.
+            if (cur_data == '#') { 
+                for (; iter == data.end(); ++iter) { 
+                    if (cur_data == ' ' || 
+                        cur_data == '\n' ||
+                        cur_data == '\r') {
+                        // finish 
+                        return std::make_tuple(token::comment, result);
+                    }
+                }
+            }
+
+            result += cur_data;
+        }
+
+
+        return std::make_tuple(token::error, result);
     }
 };
 
